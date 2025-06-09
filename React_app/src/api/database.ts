73fe6562c988aad8d1afede44a61875dd9088d84
jardel-mhg <jardel@mindhive.co.nz>
2025-06-09 11:50:22 -0300

    auto refresh when new event

--- React_app/src/api/database.ts
+++ React_app/src/api/database.ts
@@ -1,6 +1,7 @@
 import PouchDB from 'pouchdb';
 import PouchDBFind from 'pouchdb-find';
 import { useMessageStore } from '../stores/messageStore';
+import { parseMessage } from '../stores/messageStore';
 
 PouchDB.plugin(PouchDBFind);
 
@@ -9,6 +10,23 @@ export const localDB = new PouchDB('lindsay');
 export const remoteDB = new PouchDB('https://admin:wyrd@db.vpn.ind.br/mqtt_data', { skip_setup: true });
 
 console.log('[Database] Starting sync...');
+
+// Set up changes listener for local database
+localDB.changes({
+  since: 'now',
+  live: true,
+  include_docs: true
+}).on('change', (change) => {
+  if (change.doc && !change.doc._id.startsWith('_design/')) {
+    const msgObj = parseMessage(change.doc);
+    if (msgObj) {
+      console.log('[Database] New document change:', change.doc);
+      useMessageStore.getState().upsertMessage(change.doc._id, msgObj);
+    }
+  }
+}).on('error', (err) => {
+  console.error('[Database] Changes feed error:', err);
+});
 
 // Start sync
 const syncHandler = localDB.sync(remoteDB, {
