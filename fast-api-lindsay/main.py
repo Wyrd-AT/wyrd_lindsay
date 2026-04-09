e736aa57d4557c67eac4fafe715cfc44d9f8f4e2
julio <julio.paz@wyrd.com.br>
2026-04-09 14:33:43 -0300

    refatoracao

--- fast-api-lindsay/main.py
+++ fast-api-lindsay/main.py
@@ -137,7 +137,13 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Em produção, usar lista específica
+    allow_origins=[
+        "https://lindsay.vpn.ind.br",
+        "https://www.lindsay.vpn.ind.br",
+        settings.FRONTEND_URL,
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
